d07dd5fd6603ba0df1848e5dedcbb24c254b1bcb
Jostin Pizarro <jospringa@gmail.com>
2022-09-29 18:40:59 -0600

    Practica 02 inicializacion del proyecto

---
+++ src/test/java/com/practica_02/practica_02/Practica02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.practica_02.practica_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practica02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
